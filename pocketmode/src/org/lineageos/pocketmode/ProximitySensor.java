LeChenOS/device_oneplus_msm8998-common-2

msm8998-common: pocketmode: Add local FileUtils
* We don't really need lineageos sdk for one class

Change-Id: I5e06015c8ea65399bb3643183dd17641ca0d3ad7

pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
@@ -24,7 +24,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
-import org.lineageos.internal.util.FileUtils;
+import com.android.internal.util.aosip.FileUtils;
 
 public class ProximitySensor implements SensorEventListener {
 

DerpFest-Ten/latest_device_oneplus_msm8998-common

pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
@@ -24,7 +24,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
-import org.lineageos.internal.util.FileUtils;
+import com.android.internal.util.derp.FileUtils;
 
 public class ProximitySensor implements SensorEventListener {
 